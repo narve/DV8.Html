03b8080e6160cce5d6f4bf27d60ed9ad8a688611
Narve Sætre <narve@dv8.no>
2021-03-05 16:02:19 +0100

    add time element

---
+++ DV8.Html/Elements/Time.cs
@@ -0,0 +1,13 @@
+namespace DV8.Html.Elements
+{
+    public class Time : HtmlElement
+    {
+        [Attr] public string Datetime { get; set; }
+
+        public Time(string isoVersion, string textVersion = null)
+        {
+            Datetime = isoVersion;
+            Text = textVersion ?? isoVersion;
+        }
+    }
+}
