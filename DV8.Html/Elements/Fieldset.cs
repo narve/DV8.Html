f8931bf67ca7e1eb13f6cfd8c010cd7eba38315d
Narve Sætre <narve@dv8.no>
2021-04-07 14:57:56 +0200

    add name to fieldset

--- DV8.Html/Elements/Fieldset.cs
+++ DV8.Html/Elements/Fieldset.cs
@@ -6,13 +6,18 @@ namespace DV8.Html.Elements
 {
     public class Fieldset : HtmlElement
     {
+        [Attr]
+        public string Name { get; set; }
+        
         public Fieldset()
         {
         }
 
         public Fieldset(string legend, IEnumerable<IHtmlElement> subs = null)
         {
+            Name = legend;
             Subs = (subs ?? new IHtmlElement[0]).With(new Legend(legend)).ToArray();
         }
+
     }
 }
