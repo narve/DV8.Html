8e32b9723c76647263a4a2458b6553bf8ab30bd5
Narve Sætre <narve@dv8.no>
2021-04-07 14:54:31 +0200

    add withclz helper method

---
+++ DV8.Html/Support/AttributeExtensions.cs
@@ -0,0 +1,14 @@
+using DV8.Html.Elements;
+
+namespace DV8.Html.Support
+{
+    public static class AttributeExtensions
+    {
+
+        public static T WithClz<T>(this T t, string clz) where T : IHtmlElement
+        {
+            t.Clz = string.IsNullOrEmpty(t.Clz) ? clz : (t.Clz + " " + clz);
+            return t;
+        }
+    }
+}
